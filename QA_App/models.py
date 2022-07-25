e01dc9ffe78260bf6619157240340bc63e651e13
APP2121 <atulprakash2121@gmail.com>
2022-07-25 14:37:47 +0530

    created the question and answer model with UI and API

--- QA_App/models.py
+++ QA_App/models.py
@@ -1,3 +1,44 @@
+from enum import auto
 from django.db import models
 
 # Create your models here.
+
+class question_model(models.Model):
+    auto_question_id = models.AutoField(primary_key= True)
+    question_title =models.CharField(max_length=200)
+    question_description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.auto_question_id)
+
+class answer_model(models.Model):
+    auto_answer_id = models.AutoField(primary_key= True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    like_and_dislike = models.IntegerField(null=True)
+    questions_answers = models.ManyToManyField(question_model)
+
+    def __str__(self):
+        return str(self.auto_answer_id)
+
+
+class status(models.Model):
+    status_id = models.IntegerField(primary_key=True)
+    status_name = models.CharField(max_length=10)
+    question_status = models.OneToOneField(question_model, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.status_id)
+
+class technology(models.Model):
+    technology_id = models.IntegerField(primary_key=True)
+    technology_name = models.CharField (max_lenth = 20)
+    question_technology = models.ForeignKey (question_model, related_name='question_technology', on_delete=models.CASCADE)
+    answer_technology = models.ForeignKey (answer_model, related_name='answer_technology', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.technology_id)
+
+
+    
--- QA_System/settings.py
+++ QA_System/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPTALE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static' )
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'QA_App',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'QA_System.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPTALE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,13 +77,7 @@ WSGI_APPLICATION = 'QA_System.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
